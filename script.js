42556ccac3c1c790f17745f7d490ac43c0d89973
khalfh19 <zakinakbns@gmail.com>
2021-07-22 20:23:14 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// DISABLE RIGHT CLICK
+document.addEventListener("contextmenu", function(e){
+    e.preventDefault();
+}, false);
+
+// END
+
+//DISABLE DRAG IMAGE
+window.ondragstart = function() { return false; }
+
+//END
+
+
+
+
+
+
